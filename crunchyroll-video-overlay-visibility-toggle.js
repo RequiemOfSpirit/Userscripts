f2adf8649b8aafcf0e01c94dd5bc019d4d7f8fbb
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2023-11-04 15:12:50 -0700

    feat: Add Crunchyroll video overlay visibility toggle userscript

---
+++ crunchyroll-video-overlay-visibility-toggle.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Crunchyroll video overlay visibility toggle
+// @namespace    https://github.com/RequiemOfSpirit
+// @version      1.0.0
+// @description  Adds an event listener on the letter `h` to toggle video overlay visibility
+// @author       RequiemOfSpirit
+// @homepage     https://github.com/RequiemOfSpirit/Userscripts
+// @match        https://static.crunchyroll.com/vilos-*
+// @license      MIT
+// @icon         https://upload.wikimedia.org/wikipedia/commons/7/75/Cib-crunchyroll_%28CoreUI_Icons_v1.0.0%29_orange.svg
+// ==/UserScript==
+ 
+(function() {
+  'use strict';
+
+  document.addEventListener('keyup', (e) => {
+    const overlay = document.querySelector("#velocity-controls-package");
+    if (overlay == undefined || e.key !== 'h') {
+      return;
+    }
+
+    overlay.style.opacity = (overlay.style.opacity === '0') ? 1 : 0;
+  });
+})();
